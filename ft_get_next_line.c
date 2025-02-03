30b40304d8dcde86f890d885fcd44fef1fb99459
Arthur Penel <arpenel@bess-f3r6s2.clusters.42paris.fr>
2025-02-03 14:05:36 +0100

    Extracting line function added

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -18,20 +18,38 @@
 #define BUFFER_SIZE 1024
 
 // 1. Read the file and stock the data 
-char	*read_n_stock(int fd, static char *storage)
+char	*readbuf(int fd, static char *storage)
 {
 	char	buffer;
-	int		byte_read;
+	int		bytes_read;
 
-	buffer + BUFFER_ZISE + 1;
+	buffer = BUFFER_SZIE + 1;
 	bytes_read = 1;
-	while (!ft_strchr(storage, '\n') && bytes_read > 0)
+	while (!ft_strchr(storage, '\n') && bytes_read > 0) // Read til we find a \n
 	{
-		buffer 
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (NULL);
+		buffer[bytes_read] = '\0'; // Trasnform buffer as a valid string.
+		storage = ft_strjoin(storage, buffer); // Cat the two strings
 	}
+	return (storage);
 }
 
 // 2. Extract the line 
+char *extract_line(char *storage )
+{
+	char	*line;
+	char	*i;
+	
+	i = 0;
+	if(!storage)
+		return (NULL);
+	while (storage[i] && storage[i] != '\n')
+		i++;
+	ft_substr(storage, 0, i + 1);
+	return (line);
+}
 // 3. Update storage variable
 // 4. Return the lign that we just extract
 
